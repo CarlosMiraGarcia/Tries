b973306dd65898524518f30c3b962b2a5dca0a34
CarlosMiraGarcia <carlosgarciamira@gmail.com>
2020-12-31 14:43:32 +0000

    Add project files.

---
+++ Tries/Node.cs
@@ -0,0 +1,46 @@
+﻿namespace Tries {
+    public class Node {
+        private int Size { get; set; } = 0;
+        private static int NumberOfCharacters = 26;
+        Node[] Children = new Node[NumberOfCharacters];
+        private int GetCharIndex(char c) {
+            return c - 97;
+        }
+        private Node GetNode (char c) {
+            return Children[GetCharIndex(c)];
+        }
+        private void SetNode(char c, Node node) {
+            Children[GetCharIndex(c)] = node;
+        }
+        public void Add(string s) {
+            s = s.ToLower();
+            Add(s, 0);
+        }
+        private void Add(string s, int index) {
+            Size++;
+            if (index == s.Length) {
+                return;
+            }
+            char current = s[index];
+            Node child = GetNode(current);
+            if (child == null) {
+                child = new Node();
+                SetNode(current, child);
+            }
+            child.Add(s, index + 1);
+        }
+        public int FindCount(string s, int index) {
+            s = s.ToLower();
+            if (index == s.Length) {
+                return Size;
+            }
+
+            Node child = GetNode(s[index]);
+            if (child == null) {
+                return 0;
+            }
+
+            return child.FindCount(s, index + 1);            
+        }
+    }
+}
---
+++ Tries/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tries {
+    class Program {
+        static void Main(string[] args) {
+            // This will help when we have a contact list and they ask us
+            // how many of the contacts start with certain characters ('c' or 'ca', for example)
+            Node newTries = new Node();
+            newTries.Add("Carlos");
+            newTries.Add("Anna");
+            newTries.Add("Hanna");
+            newTries.Add("Brona");
+            newTries.Add("Bruno");
+            newTries.Add("Bronislava");
+            newTries.Add("Baron");
+            // FindCount will find how many strings start with "Br". 0 is the
+            // index where to start looking for.
+            Console.WriteLine(newTries.FindCount("Br", 0));
+            Console.ReadLine();
+        }
+    }
+}
